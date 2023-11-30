1f91c4227a51e503ec4d6d38b657b8d5c3836469
Natthawut Jampasaeng <121080181+Nattawut-jps@users.noreply.github.com>
2023-11-30 13:22:14 +0700

    30/11/2023

--- model/Favorite.js
+++ model/Favorite.js
@@ -6,3 +6,44 @@ const sequelize = new Sequelize({
     password: 'root',
     dialect: 'mysql',
 });
+
+const Favorite = sequelize.define('favorite', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    categories: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        defaultValue: 1,
+        allowNull: true,
+    },
+    imgURL: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    pid: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    uid: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+});
+
+(async () => {
+    await sequelize.sync({ force: false })
+})();
+module.exports = { Favorite }
--- router/CartandFavorite.js
+++ router/CartandFavorite.js
@@ -1,44 +1,129 @@
 const express = require('express');
 const route = express.Router();
 const { Cart } = require('../model/Cart');
+const { Favorite } = require('../model/Favorite');
 
-route.get('/', async (req, res, next) => {
-    const cart = await Cart.findAll();
-    res.send(cart)
+// Cart
+route.get('/cart', async (req, res, next) => {
+    res.send(await Cart.findAll());
 });
-route.post('/increaseCart', async (req, res, next) => {
+route.post('/addTocart', async (req, res, next) => {
     const { id, name, price, categories, rating, uid } = req.body;
-    const data = await Cart.findOne({ where: { pid: id } });
-    if (data) {
-        await Cart.update({
-            price: price + data.dataValues.price,
-            quantity: data.dataValues.quantity + 1,
-        }, {
-            where: {
-                uid: id
-            }
-        })
-    } else {
+    if (req.body) {
         await Cart.create({
             name: name,
             price: price,
             categories: categories,
             rating: rating,
-            imgURL: 'http://localhost:8080/img/Tometo.png',
+            imgURL: '/img/Tometo.png',
             pid: id,
             uid: uid
+        }).then(async () => {
+            res.send(await Cart.findAll())
+        });
+    } else {
+        res.errored.message;
+    };
+});
+route.post('/increase', async (req, res, next) => {
+    const { pid, price } = req.body;
+    const data = await Cart.findOne({ where: { pid: pid } });
+    if (data) {
+        await Cart.update({
+            price: data.dataValues.price + price,
+            quantity: data.dataValues.quantity + 1,
+        }, {
+            where: {
+                pid: pid
+            }
+        }).then(async () => {
+            res.send(await Cart.findAll())
         });
     }
-    res.send(await Cart.findAll());
-});
-
+})
 route.post('/decrease', async (req, res, next) => {
+    const { pid, price } = req.body;
+    const data = await Cart.findOne({ where: { pid: pid } });
+    if (data) {
+        await Cart.update({
+            price: data.dataValues.price === price ? price : data.dataValues.price - price,
+            quantity: data.dataValues.quantity === 1 ? 1 : data.dataValues.quantity - 1,
+        }, {
+            where: {
+                pid: pid
+            }
+        }).then(async () => {
+            res.send(await Cart.findAll())
+        });
+    }
+})
+
+route.post('/remove', async (req, res, next) => {
     const { id } = req.body;
     if (await Cart.findOne({ where: { pid: id } })) {
-        await Cart.destroy(
-            { where: { pid: id }, force: true })
+        await Cart.destroy({
+            where: { pid: id },
+            force: true
+        }).then(async () => {
+            res.send(await Cart.findAll())
+        });
+    } else {
+        res.errored.message;
+    }
+});
+
+route.post('/removeAll', async (req, res, next) => {
+    const { check } = req.body;
+    if (check) {
+        await Cart.destroy({ truncate: true }).then(async () => {
+            res.send(await Cart.findAll())
+        });
+    }
+});
+
+// Favorite
+route.get('/favorite', async (req, res, next) => {
+    res.send(await Favorite.findAll());
+});
+route.post('/addFavorite', async (req, res, next) => {
+    const { id, name, price, categories, rating, uid } = req.body;
+    if (req.body) {
+        await Favorite.create({
+            name: name,
+            price: price,
+            categories: categories,
+            rating: rating,
+            imgURL: '/img/Tometo.png',
+            pid: id,
+            uid: uid
+        }).then(async () => {
+            res.send(await Favorite.findAll())
+        });
+    } else {
+        res.errored.message;
     };
-    res.send(await Cart.findAll());
+});
+route.post('/removeFavorite', async (req, res, next) => {
+    const { id } = req.body;
+    if (await Favorite.findOne({ where: { pid: id } })) {
+        await Favorite.destroy({
+            where: { pid: id },
+            force: true
+        }).then(async () => {
+            res.send(await Favorite.findAll())
+        });
+    } else {
+        res.errored.message;
+    }
+})
+
+route.post('/removeAllFavorite', async (req, res, next) => {
+    const { check } = req.body;
+    if (check) {
+        await Favorite.destroy({ truncate: true }).then(async () => {
+            res.send(await Favorite.findAll())
+        });
+    }
 });
 
 module.exports = route;
