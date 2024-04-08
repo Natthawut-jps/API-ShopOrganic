39024f506c73ece0becc056014e38e31a1d54c50
Natthawut Jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2024-04-08 08:53:37 +0700

    update

--- model/_Database_Connected.js
+++ model/_Database_Connected.js
@@ -1,12 +1,8 @@
 const { Sequelize } = require("sequelize");
 
 // Client-Side
-const sequelize = new Sequelize({
-  host: "dpg-co9kl04f7o1s739aadtg-a",
-  database: "shops_yjan",
-  username: 'shops_yjan_user',
-  password: 'iW8qTfq9RbhudhrzKwiSVPyYHonBjI1a',
-  dialect: "postgres",
-  port: '5432',
-});
+const sequelize = new Sequelize(
+  "postgres://shops_yjan_user:iW8qTfq9RbhudhrzKwiSVPyYHonBjI1a@dpg-co9kl04f7o1s739aadtg-a.singapore-postgres.render.com/shops_yjan"
+);
+
 module.exports["sequelize"] = sequelize;
