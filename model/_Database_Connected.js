1e7ac62475c5e95dc1f24269b118dc2632627665
Natthawut Jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2024-04-08 16:24:18 +0700

    up

--- model/_Database_Connected.js
+++ model/_Database_Connected.js
@@ -2,7 +2,10 @@ const { Sequelize } = require("sequelize");
 
 // Client-Side
 const sequelize = new Sequelize(
- "postgres://shops_0kkg_user:W3IJ5Cyjvq4E04oxGKyIS3kGxs1hDj0Z@dpg-co9qiasf7o1s739d8bm0-a/shops_0kkg"
+{
+    dialect: "sqlite",
+    storage: "./DB/sqlite.db"
+}
 );
 
 module.exports["sequelize"] = sequelize;
