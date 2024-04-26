e90347ce7096bcb23be7a2316de789f0e2d641c1
Natthawut Jampasaeng <121080181+Natthawut-jps@users.noreply.github.com>
2024-04-26 17:16:36 +0700

    26/04/2024

--- model/_Database_Connected.js
+++ model/_Database_Connected.js
@@ -2,10 +2,7 @@ const { Sequelize } = require("sequelize");
 
 // Client-Side
 const sequelize = new Sequelize(
-{
-    dialect: "sqlite",
-    storage: "./DB/sqlite.db"
-}
+  "mysql://avnadmin:AVNS_QZSJ00OV-68XQ5kyRrV@mysql-3700c843-natthawut-70ad.d.aivencloud.com:24939/defaultdb?ssl-mode=REQUIRED"
 );
 
 module.exports["sequelize"] = sequelize;
